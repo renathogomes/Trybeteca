3c0b293ebd895c71ca5066440e9e5318561157bc
Renato Filho <renatosousafilho@gmail.com>
2023-04-06 15:10:01 -0300

    aplicando ajustes no Dockerfile e docker-compose.yml

--- src/database/config/database.ts
+++ src/database/config/database.ts
@@ -2,7 +2,7 @@ import { Options } from 'sequelize';
 
 const config: Options = {
   username: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || 'password',
+  password: process.env.DB_PASSWORD || 'password',
   database: process.env.DB_NAME || 'transactions_api',
   host: process.env.DB_HOST || 'localhost',
   port: Number(process.env.DB_PORT) || 3306,
