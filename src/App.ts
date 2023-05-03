fe8e78bb792060e2ce6df346db5a16f3cee9189d
Jonathan Ferreira <j0n4t@hotmail.com>
2023-05-03 10:51:22 -0300

    feat: App e server

---
+++ src/App.ts
@@ -0,0 +1,22 @@
+import express = require('express');
+import 'express-async-errors';
+
+class App {
+  public app: express.Express;
+
+  constructor() {
+    this.app = express();
+
+    this.app.use(express.json());
+
+    // Não remover essa rota
+    this.app.get('/', (_req, res) => res.status(200).send('Trybeteca API no ar!'));
+  }
+
+
+  public start(PORT: string | number):void {
+    this.app.listen(PORT, () => console.log(`Trybeteca API no ar na porta ${PORT}!`));
+  }
+}
+
+export default App;
